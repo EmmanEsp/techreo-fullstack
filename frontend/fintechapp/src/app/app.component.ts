975b6b1771ed80ab8f39320aa9cba0a600e8fde3
EmmanEsp <emmanesp13@gmail.com>
2024-09-04 16:31:00 -0700

    improved readability

--- frontend/fintechapp/src/app/app.component.ts
+++ frontend/fintechapp/src/app/app.component.ts
@@ -1,66 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-
-interface CreateCustomerResponse {
-  customerId: string;
-}
-
-export interface CreateAccountResponse{
-  accountName: string;
-  clabe: string;
-  balance: number;
-}
-
-interface SigninResponse {
-  customerId: string;
-  name: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  accountNumber: string;
-  clabe: string;
-  balance: number;
-}
-
-interface TransactionResponse {
-  type: string;
-  amount: number;
-  createdAt: string; // ISO 8601 date format
-}
-
-interface ServiceResponse<T> {
-  status: string;
-  data: T;
-}
+import { CreateCustomerResponse, SigninResponse, TransactionResponse, ServiceResponse } from './models'; // Import models
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, MatCardModule, CommonModule],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fintechapp';
-  
-  customer: SigninResponse | undefined;
-  transactions: TransactionResponse[] = [];
 
-  showSigninForm: boolean = true;
-  showRegistrationForm: boolean = false;
-  showMainContent: boolean = false;
-
-  constructor(private http: HttpClient) { }
-
-  registrationForm = new FormGroup({
+  registrationForm: FormGroup = new FormGroup({
     name: new FormControl(''),
     lastName: new FormControl(''),
     email: new FormControl(''),
@@ -68,112 +27,136 @@ export class AppComponent {
     password: new FormControl('')
   });
 
-  signinForm = new FormGroup({
+  signinForm: FormGroup = new FormGroup({
     user: new FormControl(''),
-    password: new FormControl(''),
+    password: new FormControl('')
   });
 
-  depositForm = new FormGroup({
-    amount: new FormControl('')
-  });
-  
-  withdrawForm  = new FormGroup({
+  depositForm: FormGroup = new FormGroup({
     amount: new FormControl('')
   });
 
-  createAccount(customerId: string) {
-    const body = { customerId: customerId };
+  withdrawForm: FormGroup = new FormGroup({
+    amount: new FormControl('')
+  });
+
+  // State
+  customer?: SigninResponse;
+  transactions: TransactionResponse[] = [];
+  showSigninForm = true;
+  showRegistrationForm = false;
+  showMainContent = false;
+
+  constructor(private http: HttpClient) {}
+
+  // ------------------ Account Methods ------------------
+
+  /**
+   * Creates a new account for the customer.
+   */
+  private createAccount(customerId: string) {
+    const body = { customerId };
     this.http.post("http://localhost:5284/api/v1/account", body).subscribe({
-      next: (response) => console.log('Success:', response),
+      next: () => console.log('Account creation successful'),
       error: (error) => console.error('Error:', error)
     });
   }
 
-  getAllTransaction(customerId: string) {
+  /**
+   * Fetches all transactions for the given customer.
+   */
+  private getAllTransactions(customerId: string) {
     this.http.get<ServiceResponse<TransactionResponse[]>>(`http://localhost:5284/api/v1/transaction/customer/${customerId}`).subscribe({
       next: (response) => this.transactions = response.data,
       error: (error) => console.error('Error:', error)
     });
   }
 
+  // ------------------ Form Submit Handlers ------------------
+
+  /**
+   * Handles the registration form submission.
+   */
   onSubmitRegistration() {
     this.http.post<ServiceResponse<CreateCustomerResponse>>('http://localhost:5284/api/v1/customer', this.registrationForm.value).subscribe({
       next: (response) => {
         this.createAccount(response.data.customerId);
-        this.showRegistrationForm = false;
-        this.showSigninForm = true;
+        this.toggleLoginRegistration();
         this.registrationForm.reset();
       },
       error: (error) => console.error('Error:', error)
     });
   }
 
+  /**
+   * Handles the sign-in form submission.
+   */
   onSubmitSignin() {
     this.http.post<ServiceResponse<SigninResponse>>('http://localhost:5284/api/v1/signin', this.signinForm.value).subscribe({
       next: (response) => {
         this.customer = response.data;
-        this.getAllTransaction(response.data.customerId);
+        this.getAllTransactions(response.data.customerId);
         this.showSigninForm = false;
-        this.showMainContent = true; 
+        this.showMainContent = true;
       },
       error: (error) => console.error('Error:', error)
     });
   }
 
+  /**
+   * Handles the deposit form submission.
+   */
+  onSubmitDeposit() {
+    if (!this.customer) return;
+
+    const body = { ...this.depositForm.value, customerId: this.customer.customerId };
+    this.http.post<ServiceResponse<TransactionResponse>>('http://localhost:5284/api/v1/transaction/deposit', body).subscribe({
+      next: (response) => {
+        if(this.customer != null) {
+          this.customer.balance += response.data.amount;
+          this.transactions.unshift(response.data);
+          this.depositForm.reset();
+        }
+      },
+      error: (error) => console.error('Error:', error)
+    });
+  }
+
+  /**
+   * Handles the withdraw form submission.
+   */
+  onSubmitWithdraw() {
+    if (!this.customer) return;
+
+    const body = { ...this.withdrawForm.value, customerId: this.customer.customerId };
+    this.http.post<ServiceResponse<TransactionResponse>>('http://localhost:5284/api/v1/transaction/withdraw', body).subscribe({
+      next: (response) => {
+        if(this.customer != null) {
+          this.customer.balance -= response.data.amount;
+          this.transactions.unshift(response.data);
+          this.withdrawForm.reset();
+        }
+      },
+      error: (error) => console.error('Error:', error)
+    });
+  }
+
+  // ------------------ Utility Methods ------------------
+
+  /**
+   * Toggles between login and registration forms.
+   */
   toggleLoginRegistration() {
     this.showSigninForm = !this.showSigninForm;
     this.showRegistrationForm = !this.showRegistrationForm;
   }
-  
+
+  /**
+   * Logs out the user and resets the customer information.
+   */
   logout() {
+    this.customer = undefined;
     this.showSigninForm = true;
     this.showMainContent = false;
-    this.customer = undefined; // Reset customer information
-  }
-
-  onSubmitDeposit() {
-    if (!this.customer) {
-      console.error('Customer information is not available.');
-      return;
-    }
-    const body = {
-      ...this.depositForm.value,
-      customerId: this.customer.customerId
-    };
-    this.http.post<ServiceResponse<TransactionResponse>>('http://localhost:5284/api/v1/transaction/deposit', body).subscribe({
-      next: (response) => {
-        if (!this.customer) {
-          console.error('Customer information is not available.');
-          return;
-        }
-        this.customer.balance += response.data.amount;
-        this.transactions.unshift(response.data);
-        this.depositForm.reset();
-      },
-      error: (error) => console.error('Error:', error)
-    });
-  }
-
-  onSubmitWithdraw() {
-    if (!this.customer) {
-      console.error('Customer information is not available.');
-      return;
-    }
-    const body = {
-      ...this.withdrawForm.value,
-      customerId: this.customer.customerId
-    };
-    this.http.post<ServiceResponse<TransactionResponse>>('http://localhost:5284/api/v1/transaction/withdraw', body).subscribe({
-      next: (response) => {
-        if (!this.customer) {
-          console.error('Customer information is not available.');
-          return;
-        }
-        this.customer.balance -= response.data.amount;
-        this.transactions.unshift(response.data);
-        this.withdrawForm.reset();
-      },
-      error: (error) => console.error('Error:', error)
-    });
   }
 }
