545eed732c1daed9a4d305fb7d9068baeb7fe1d5
EmmanEsp <emmanesp13@gmail.com>
2024-09-04 16:31:00 -0700

    improved readability

---
+++ frontend/fintechapp/src/app/models.ts
@@ -0,0 +1,25 @@
+export interface CreateCustomerResponse {
+    customerId: string;
+}
+
+export interface SigninResponse {
+    customerId: string;
+    name: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    accountNumber: string;
+    clabe: string;
+    balance: number;
+}
+
+export interface TransactionResponse {
+    type: string;
+    amount: number;
+    createdAt: string;
+}
+
+export interface ServiceResponse<T> {
+    status: string;
+    data: T;
+}
